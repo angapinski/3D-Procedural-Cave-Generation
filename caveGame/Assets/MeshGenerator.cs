750e4e48f96c1ed80e4922dd2ef40489037225ce
Andrew Gapinski <angapinski@unomaha.edu>
2018-09-09 12:51:21 -0500

    Update to new Unity version, Additional changes
    Progress on Map and Mesh Generation

---
+++ caveGame/Assets/MeshGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour {
+
+    public SquareGrid squareGrid;
+
+    public void GenerateMesh(int[,] map, float squareSize)
+    {
+        squareGrid = new SquareGrid(map, squareSize);
+    }
+
+    public class SquareGrid {
+        public Square[,] squares;
+
+        public SquareGrid(int[,] map, float squareSize) {
+            int nodeCountX = map.GetLength(0);
+            int nodeCountY = map.GetLength(1);
+            float mapWidth = nodeCountX * squareSize;
+            float mapHeight = nodeCountY * squareSize;
+        }
+    }
+
+    public class Square {
+        public ControlNode topLeft, topRight, bottomRight, bottomLeft;
+        public Node midTop, midRight, midBottom, midLeft;
+
+        public Square(ControlNode topLeft, ControlNode topRight, ControlNode bottomRight, ControlNode bottomLeft) {
+            this.topLeft = topLeft;
+            this.topRight = topRight;
+            this.bottomRight = bottomRight;
+            this.bottomLeft = bottomLeft;
+
+            midTop = topLeft.right;
+            midRight = bottomRight.above;
+            midBottom = bottomLeft.right;
+            midLeft = bottomLeft.above;
+        }
+    }
+
+	public class Node {
+        public Vector3 position;
+        public int vertex = -1;
+
+        public Node(Vector3 pos) {
+            position = pos;
+        }
+    }
+
+    public class ControlNode : Node
+    {
+        public bool active;
+        public Node above, right;
+
+        public ControlNode(Vector3 pos, bool active, float squareSize) : base(pos) {
+            this.active = active;
+            above = new Node(pos + Vector3.forward * squareSize / 2f);
+            above = new Node(pos + Vector3.right * squareSize / 2f);
+        }
+    }
+}
